ce06fbcae1212b6ba655eac4a52b2819aed204cb
Kenta Katsura <katsura.rs@gmail.com>
2025-10-02 14:03:07 +0900

    update docs

--- docs/classskyway_1_1room_1_1interface_1_1_room.js
+++ docs/classskyway_1_1room_1_1interface_1_1_room.js
@@ -1,19 +1,18 @@
 var classskyway_1_1room_1_1interface_1_1_room =
 [
     [ "EventListener", "classskyway_1_1room_1_1interface_1_1_room_1_1_event_listener.html", "classskyway_1_1room_1_1interface_1_1_room_1_1_event_listener" ],
-    [ "~Room", "classskyway_1_1room_1_1interface_1_1_room.html#abab7c1ddef326b460fceea46b9776b70", null ],
     [ "AddEventListener", "classskyway_1_1room_1_1interface_1_1_room.html#aec757a42c895920f05b576c8797b19cb", null ],
     [ "Close", "classskyway_1_1room_1_1interface_1_1_room.html#a1da8a4a619516ab94a794d1e477147c4", null ],
     [ "Dispose", "classskyway_1_1room_1_1interface_1_1_room.html#a0387f196e74d01416feb8c0e71193bbc", null ],
     [ "Id", "classskyway_1_1room_1_1interface_1_1_room.html#ab9fd9338c934f52e7c4d4dbf12ffd357", null ],
     [ "Leave", "classskyway_1_1room_1_1interface_1_1_room.html#a8556352393af87ca76fbfcf91dce68e3", null ],
-    [ "Members", "classskyway_1_1room_1_1interface_1_1_room.html#a47eed73b6cd07d26006e16da0ad6ab83", null ],
-    [ "Metadata", "classskyway_1_1room_1_1interface_1_1_room.html#ad3377438df8b446d5519dd4649bc6398", null ],
-    [ "Name", "classskyway_1_1room_1_1interface_1_1_room.html#a7063f1b37add599f7c40232d4efabe3e", null ],
-    [ "Publications", "classskyway_1_1room_1_1interface_1_1_room.html#ab4126f15dc5a6bf10b760e77bed95fff", null ],
+    [ "Members", "classskyway_1_1room_1_1interface_1_1_room.html#a6d5fdc9d816134ad3e2f488be9b53a20", null ],
+    [ "Metadata", "classskyway_1_1room_1_1interface_1_1_room.html#aea591b3a94a7c5cf4c6fc6c692f8d859", null ],
+    [ "Name", "classskyway_1_1room_1_1interface_1_1_room.html#a5ec1339733e2122506e7daa4004c1eb8", null ],
+    [ "Publications", "classskyway_1_1room_1_1interface_1_1_room.html#a01feb404d179fae08cf2137dafa15690", null ],
     [ "RemoveEventListener", "classskyway_1_1room_1_1interface_1_1_room.html#a6ba6cd3eec8c322dfd01723fac508627", null ],
     [ "State", "classskyway_1_1room_1_1interface_1_1_room.html#af23ddc51289e52b1f41dcdd5e15cf783", null ],
-    [ "Subscriptions", "classskyway_1_1room_1_1interface_1_1_room.html#ab63612840980906599861908bb8f86a7", null ],
+    [ "Subscriptions", "classskyway_1_1room_1_1interface_1_1_room.html#a93654c6ba6f86e36eb256f6e181be293", null ],
     [ "Type", "classskyway_1_1room_1_1interface_1_1_room.html#ab883b23bb7bc67e52c1b347c75ba7e2e", null ],
     [ "UpdateMetadata", "classskyway_1_1room_1_1interface_1_1_room.html#a795ea36406bb887202b395e82e86b37e", null ]
 ];
--- docs/structskyway_1_1core_1_1_context_options_1_1_token.js
+++ docs/structskyway_1_1core_1_1_context_options_1_1_token.js
@@ -1,6 +1,4 @@
 var structskyway_1_1core_1_1_context_options_1_1_token =
 [
-    [ "listener", "structskyway_1_1core_1_1_context_options_1_1_token.html#a32e54bab96bea870fe48a09a67a9701e", null ],
-    [ "remind_time_sec", "structskyway_1_1core_1_1_context_options_1_1_token.html#af019a7cff4710a053c4d63ba6743f27b", null ],
-    [ "update_remind_sec", "structskyway_1_1core_1_1_context_options_1_1_token.html#a5aac13d9d006ded714ced255119b7017", null ]
+    [ "remind_time_sec", "structskyway_1_1core_1_1_context_options_1_1_token.html#af019a7cff4710a053c4d63ba6743f27b", null ]
 ];
